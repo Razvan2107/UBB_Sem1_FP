b1b740345654ea475eba7461db7c838b56cb4857
Razvan2107 <123764280+Razvan2107@users.noreply.github.com>
2024-03-04 21:18:32 +0200

    Add files via upload

---
+++ Backtracking/Problem.py
@@ -0,0 +1,44 @@
+def is_consistent(x,n):
+    if len(x)>n:
+        return False
+    if len(x)!=len(set(x)):
+        return False
+    for i in range(1,len(x)):
+        ok = False
+        for j in range(0,i):
+            if abs(x[i]-x[j])==1:
+                ok = True
+        if ok==False:
+            return False
+    return True
+
+def is_solution(x,n):
+    return len(x)==n
+
+def back_rec(x,n):
+    x.append(-1)
+    for i in range (1,n+1):
+        x[-1]=i
+        if is_consistent(x,n):
+            if is_solution(x,n):
+                print(x)
+            back_rec(x[:],n)
+
+def back_iter(n):
+    x=[0]
+    while len(x)>0:
+        chosen=False
+        while not chosen and x[-1]<n:
+            x[-1]+=1
+            chosen=is_consistent(x,n)
+        if chosen:
+            if is_solution(x,n):
+                print(x)
+            x.append(0)
+        else:
+            x.pop()
+        
+
+back_rec([],5)
+print('-----')
+back_iter(5)
